27b34df1136fc4cfc938c50e8cd889bd8728009b
timmaxx <m.n.timchenko@gmail.com>
2025-03-25 17:50:54 +0300

    02 rename TimeUtil to DateTimeUtil

--- src/main/java/com/timmax/realestate/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package com.timmax.realestate.util;
-
-import java.time.LocalTime;
-
-public class TimeUtil {
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return !lt.isBefore(startTime) && lt.isBefore(endTime);
-    }
-}
