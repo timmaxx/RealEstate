ffa3b2dc34bd16c67a449c42ca7e8a925cbfcd08
timmaxx <m.n.timchenko@gmail.com>
2025-03-25 14:32:00 +0300

    01 simple logging

--- src/main/java/com/timmax/realestate/web/UserServlet.java
+++ src/main/java/com/timmax/realestate/web/UserServlet.java
@@ -1,17 +1,24 @@
 package com.timmax.realestate.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class UserServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         //  request.getRequestDispatcher("/users.jsp").forward(request, response);
+
+        log.debug("redirect to users");
         response.sendRedirect("users.jsp");
     }
 }
