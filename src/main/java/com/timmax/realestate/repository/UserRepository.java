4e52e6df2b6707c1dbbb2da33fbc655365d389ad
timmaxx <m.n.timchenko@gmail.com>
2025-03-29 15:16:02 +0300

    02 add User and UserRepository classes

---
+++ src/main/java/com/timmax/realestate/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.timmax.realestate.repository;
+
+import com.timmax.realestate.model.User;
+
+import java.util.List;
+
+public interface UserRepository {
+    // null if not found, when updated
+    User save(User user);
+
+    // false if not found
+    boolean delete(int id);
+
+    // null if not found
+    User get(int id);
+
+    // null if not found
+    User getByEmail(String email);
+
+    List<User> getAll();
+}
