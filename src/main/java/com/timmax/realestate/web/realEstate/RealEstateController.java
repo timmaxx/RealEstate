a09bbd900eaafe18cced0b094aed2cb69e664b9a
timmaxx <m.n.timchenko@gmail.com>
2025-03-29 16:02:17 +0300

    02 prepare empty controller layer for RealEstate

---
+++ src/main/java/com/timmax/realestate/web/realEstate/RealEstateController.java
@@ -0,0 +1,7 @@
+package com.timmax.realestate.web.realEstate;
+
+import com.timmax.realestate.service.RealEstateService;
+
+public class RealEstateController {
+    private RealEstateService service;
+}
