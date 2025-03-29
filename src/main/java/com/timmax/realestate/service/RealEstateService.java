2b3e12a79efaf1cad7724ba39dc94e08dd49cedf
timmaxx <m.n.timchenko@gmail.com>
2025-03-29 15:56:47 +0300

    02 prepare empty service layer for RealEstate

---
+++ src/main/java/com/timmax/realestate/service/RealEstateService.java
@@ -0,0 +1,7 @@
+package com.timmax.realestate.service;
+
+import com.timmax.realestate.repository.RealEstateRepository;
+
+public class RealEstateService {
+    private RealEstateRepository repository;
+}
