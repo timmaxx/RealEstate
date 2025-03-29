2048d8e098ab563222e492ec55d58aab6f962be2
timmaxx <m.n.timchenko@gmail.com>
2025-03-29 15:49:45 +0300

    02 add controller layer for User (with mock authorization)

---
+++ src/main/java/com/timmax/realestate/web/SecurityUtil.java
@@ -0,0 +1,7 @@
+package com.timmax.realestate.web;
+
+public class SecurityUtil {
+    public static int authUserId() {
+        return 1;
+    }
+}
---
+++ src/main/java/com/timmax/realestate/web/user/ProfileRestController.java
@@ -0,0 +1,20 @@
+package com.timmax.realestate.web.user;
+
+import com.timmax.realestate.model.User;
+
+import static com.timmax.realestate.web.SecurityUtil.authUserId;
+
+public class ProfileRestController extends AbstractUserController {
+
+    public User get() {
+        return super.get(authUserId());
+    }
+
+    public void delete() {
+        super.delete(authUserId());
+    }
+
+    public void update(User user) {
+        super.update(user, authUserId());
+    }
+}
