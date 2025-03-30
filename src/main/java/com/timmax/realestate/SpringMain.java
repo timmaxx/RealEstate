f3b900f5b718644a9205dad2695610e018cbcad5
timmaxx <m.n.timchenko@gmail.com>
2025-03-30 20:11:43 +0300

    02 add using spring-context xml file

---
+++ src/main/java/com/timmax/realestate/SpringMain.java
@@ -0,0 +1,19 @@
+package com.timmax.realestate;
+
+import com.timmax.realestate.repository.UserRepository;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
+//        UserRepository userRepository = (UserRepository) appCtx.getBean("inmemoryUserRepository");
+        UserRepository userRepository = appCtx.getBean(UserRepository.class);
+        userRepository.getAll();
+        appCtx.close();
+    }
+}
