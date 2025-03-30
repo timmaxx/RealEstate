b45ad9219883282f69427c3e0a3f7f594eab475b
timmaxx <m.n.timchenko@gmail.com>
2025-03-30 19:48:24 +0300

    02 add spring-context

--- src/main/java/com/timmax/realestate/model/User.java
+++ src/main/java/com/timmax/realestate/model/User.java
@@ -1,8 +1,8 @@
 package com.timmax.realestate.model;
 
-import java.util.Date;
-import java.util.EnumSet;
-import java.util.Set;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
 
 public class User extends AbstractNamedEntity {
     private String email;
@@ -15,16 +15,16 @@ public class User extends AbstractNamedEntity {
 
     private Set<Role> roles;
 
-    public User(Integer id, String name, String email, String password, Role role, Role... roles) {
-        this(id, name, email, password, true, EnumSet.of(role, roles));
+    public User(Integer id, String name, String email, String password, Role... roles) {
+        this(id, name, email, password, true, Arrays.asList(roles));
     }
 
-    public User(Integer id, String name, String email, String password, boolean enabled, Set<Role> roles) {
+    public User(Integer id, String name, String email, String password, boolean enabled, Collection<Role> roles) {
         super(id, name);
         this.email = email;
         this.password = password;
         this.enabled = enabled;
-        this.roles = roles;
+        setRoles(roles);
     }
 
     public String getEmail() {
@@ -57,6 +57,10 @@ public class User extends AbstractNamedEntity {
 
     public Set<Role> getRoles() {
         return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = CollectionUtils.isEmpty(roles) ? EnumSet.noneOf(Role.class) : EnumSet.copyOf(roles);
     }
 
     public String getPassword() {
