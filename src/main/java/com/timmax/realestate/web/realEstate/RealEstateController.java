475ff886fdcd6c3ecc58db40b06a4e6b9f56913b
timmaxx <m.n.timchenko@gmail.com>
2025-04-01 18:57:43 +0300

    03 HW02 implement RealEstateController

--- src/main/java/com/timmax/realestate/web/realEstate/RealEstateController.java
+++ src/main/java/com/timmax/realestate/web/realEstate/RealEstateController.java
@@ -1,7 +1,58 @@
 package com.timmax.realestate.web.realEstate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import com.timmax.realestate.model.RealEstate;
 import com.timmax.realestate.service.RealEstateService;
+import com.timmax.realestate.dto.RealEstateDto;
+import com.timmax.realestate.util.RealEstateUtil;
+import com.timmax.realestate.web.SecurityUtil;
 
+import java.util.List;
+
+import static com.timmax.realestate.util.ValidationUtil.assureIdConsistent;
+import static com.timmax.realestate.util.ValidationUtil.checkIsNew;
+
+@Controller
 public class RealEstateController {
-    private RealEstateService service;
+    private static final Logger log = LoggerFactory.getLogger(RealEstateController.class);
+
+    private final RealEstateService service;
+
+    public RealEstateController(RealEstateService service) {
+        this.service = service;
+    }
+
+    public RealEstate get(int id) {
+        int userId = SecurityUtil.authUserId();
+        log.info("get meal {} for user {}", id, userId);
+        return service.get(id, userId);
+    }
+
+    public void delete(int id) {
+        int userId = SecurityUtil.authUserId();
+        log.info("delete meal {} for user {}", id, userId);
+        service.delete(id, userId);
+    }
+
+    public List<RealEstateDto> getAll() {
+        int userId = SecurityUtil.authUserId();
+        log.info("getAll for user {}", userId);
+        return RealEstateUtil.getDtos(service.getAll(userId));
+    }
+
+    public RealEstate create(RealEstate realEstate) {
+        int userId = SecurityUtil.authUserId();
+        checkIsNew(realEstate);
+        log.info("create {} for user {}", realEstate, userId);
+        return service.create(realEstate, userId);
+    }
+
+    public void update(RealEstate realEstate, int id) {
+        int userId = SecurityUtil.authUserId();
+        assureIdConsistent(realEstate, id);
+        log.info("update {} for user {}", realEstate, userId);
+        service.update(realEstate, userId);
+    }
 }
