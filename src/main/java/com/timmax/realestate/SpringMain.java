fb71f4b8bf0bd6a05955690147b841ce92972aa5
timmaxx <m.n.timchenko@gmail.com>
2025-04-01 22:12:22 +0300

    03 HW02 add tests into SpringMain

--- src/main/java/com/timmax/realestate/SpringMain.java
+++ src/main/java/com/timmax/realestate/SpringMain.java
@@ -4,9 +4,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.timmax.realestate.model.Role;
 import com.timmax.realestate.model.User;
+import com.timmax.realestate.dto.RealEstateDto;
+import com.timmax.realestate.web.realEstate.RealEstateRestController;
 import com.timmax.realestate.web.user.AdminRestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
@@ -15,6 +18,22 @@ public class SpringMain {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
+            System.out.println();
+
+            RealEstateRestController realEstateController = appCtx.getBean(RealEstateRestController.class);
+            List<RealEstateDto> filteredMealsWithExcess =
+/*
+                    realEstateController.getBetween(
+                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
+                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0)
+                    );
+*/
+                    realEstateController.getAll();
+            filteredMealsWithExcess.forEach(System.out::println);
+            System.out.println();
+            // System.out.println(realEstateController.getBetween(null, null, null, null));
+            System.out.println(realEstateController.getAll());
         }
     }
 }
