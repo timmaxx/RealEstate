ba2eb8560354613279e12307a47ce604a5faa528
timmaxx <m.n.timchenko@gmail.com>
2025-04-03 09:25:49 +0300

    03 HW02 optional select user

--- src/main/java/com/timmax/realestate/web/SecurityUtil.java
+++ src/main/java/com/timmax/realestate/web/SecurityUtil.java
@@ -1,7 +1,13 @@
 package com.timmax.realestate.web;
 
 public class SecurityUtil {
+    private static int id = 1;
+
     public static int authUserId() {
-        return 1;
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
     }
 }
