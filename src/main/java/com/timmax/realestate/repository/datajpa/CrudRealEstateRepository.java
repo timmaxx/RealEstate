135526651dbfa6fd4a5a2e4296b7941bf4d4845e
timmaxx <m.n.timchenko@gmail.com>
2025-05-02 15:30:59 +0300

    06 01 HW5 data_jpa

--- src/main/java/com/timmax/realestate/repository/datajpa/CrudRealEstateRepository.java
+++ src/main/java/com/timmax/realestate/repository/datajpa/CrudRealEstateRepository.java
@@ -1,7 +1,49 @@
 package com.timmax.realestate.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import com.timmax.realestate.model.RealEstate;
 
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudRealEstateRepository extends JpaRepository<RealEstate, Integer> {
+    //  TimMax:
+    //      Во всех запросах есть полностью и частично повторяющиеся блоки.
+    //      А что если эти повторы вынести в отдельные строковые константы и собирать из них нужные запросы?!
+
+    @Modifying
+    @Transactional
+    @Query("""
+        DELETE
+          FROM RealEstate re
+         WHERE re.user.id = :userId
+           AND re.id = :id
+    """)
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Query("""
+        SELECT re
+          FROM RealEstate re
+         WHERE re.user.id = :userId
+         ORDER BY re.address
+    """)
+    List<RealEstate> getAll(@Param("userId") int userId);
+
+    @Query("""
+        SELECT re
+          FROM RealEstate re
+         WHERE re.user.id = :userId
+           AND re.square >= :startSquare
+           AND re.square < :endSquare
+         ORDER BY re.address
+    """)
+    List<RealEstate> getBetweenHalfOpen(
+            @Param("startSquare") Float startSquare,
+            @Param("endSquare") Float endSquare,
+            @Param("userId") int userId
+    );
 }
--- src/main/java/com/timmax/realestate/repository/datajpa/DataJpaRealEstateRepository.java
+++ src/main/java/com/timmax/realestate/repository/datajpa/DataJpaRealEstateRepository.java
@@ -1,6 +1,7 @@
 package com.timmax.realestate.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import com.timmax.realestate.model.RealEstate;
 import com.timmax.realestate.repository.RealEstateRepository;
 
@@ -9,34 +10,45 @@ import java.util.List;
 @Repository
 public class DataJpaRealEstateRepository implements RealEstateRepository {
 
-    private final CrudRealEstateRepository crudRepository;
+    private final CrudRealEstateRepository crudRealEstateRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaRealEstateRepository(CrudRealEstateRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaRealEstateRepository(
+            CrudRealEstateRepository crudRealEstateRepository,
+            CrudUserRepository crudUserRepository) {
+        this.crudRealEstateRepository = crudRealEstateRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
+    @Transactional
     public RealEstate save(RealEstate realEstate, int userId) {
-        return null;
+        if (!realEstate.isNew() && get(realEstate.id(), userId) == null) {
+            return null;
+        }
+        realEstate.setUser(crudUserRepository.getReferenceById(userId));
+        return crudRealEstateRepository.save(realEstate);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRealEstateRepository.delete(id, userId) != 0;
     }
 
     @Override
     public RealEstate get(int id, int userId) {
-        return null;
+        return crudRealEstateRepository.findById(id)
+                .filter(realEstate -> realEstate.getUser().getId() == userId)
+                .orElse(null);
     }
 
     @Override
     public List<RealEstate> getAll(int userId) {
-        return null;
+        return crudRealEstateRepository.getAll(userId);
     }
 
     @Override
     public List<RealEstate> getBetweenHalfOpen(Float startSquare, Float endSquare, int userId) {
-        return null;
+        return crudRealEstateRepository.getBetweenHalfOpen(startSquare, endSquare, userId);
     }
 }
