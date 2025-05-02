0a019d2c91e5db61b0e8d7c547f590b1bdf996de
timmaxx <m.n.timchenko@gmail.com>
2025-05-02 14:22:13 +0300

    05 08 spring_cache

--- src/main/java/com/timmax/realestate/service/UserService.java
+++ src/main/java/com/timmax/realestate/service/UserService.java
@@ -1,5 +1,7 @@
 package com.timmax.realestate.service;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import com.timmax.realestate.model.User;
@@ -17,11 +19,13 @@ public class UserService {
         this.repository = repository;
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public User create(User user) {
         Assert.notNull(user, "user must not be null");
         return repository.save(user);
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void delete(int id) {
         checkNotFound(repository.delete(id), id);
     }
@@ -35,10 +39,12 @@ public class UserService {
         return checkNotFound(repository.getByEmail(email), "email=" + email);
     }
 
+    @Cacheable("users")
     public List<User> getAll() {
         return repository.getAll();
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void update(User user) {
         Assert.notNull(user, "user must not be null");
         checkNotFound(repository.save(user), user.getId());
