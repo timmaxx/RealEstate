d8cbabafefcb6ef64833824d39c7fa4150d83451
timmaxx <m.n.timchenko@gmail.com>
2025-05-30 19:14:33 +0300

    07 13 02 jackson object mapper (date as timestamp)

--- src/main/java/com/timmax/realestate/web/json/JacksonObjectMapper.java
+++ src/main/java/com/timmax/realestate/web/json/JacksonObjectMapper.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * <p>
@@ -19,6 +21,10 @@ public class JacksonObjectMapper extends ObjectMapper {
     private JacksonObjectMapper() {
         // Регистрируем конструктор Hibernate5Module, который не делает сериализацию ленивых коллекций
         registerModule(new Hibernate5Module());
+
+        //  Теперь даты будут записываться в JSON в виде TimeStamp
+        registerModule(new JavaTimeModule());
+        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
         /*  Эти настройки - альтернатива
         @JsonAutoDetect(
