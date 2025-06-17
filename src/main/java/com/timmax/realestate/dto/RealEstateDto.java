df47c134f46fe704632f37dabb7df5a65ae43446
timmaxx <m.n.timchenko@gmail.com>
2025-06-17 22:04:21 +0300

    08 01 HW07 controller test

--- src/main/java/com/timmax/realestate/dto/RealEstateDto.java
+++ src/main/java/com/timmax/realestate/dto/RealEstateDto.java
@@ -1,5 +1,7 @@
 package com.timmax.realestate.dto;
 
+import java.util.Objects;
+
 public class RealEstateDto {
     private final Integer id;
     private final String address;
@@ -21,6 +23,20 @@ public class RealEstateDto {
 
     public float getSquare() {
         return square;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        RealEstateDto that = (RealEstateDto) o;
+        return Float.compare(square, that.square) == 0 &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, address, square);
     }
 
     @Override
--- src/test/java/com/timmax/realestate/web/RootControllerTest.java
+++ src/test/java/com/timmax/realestate/web/RootControllerTest.java
@@ -10,7 +10,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import static com.timmax.realestate.RealEstateTestData.realEstates;
 import static com.timmax.realestate.UserTestData.*;
+import static com.timmax.realestate.util.RealEstateUtil.getDtos;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -22,12 +24,30 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(view().name("users"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
                 .andExpect(model().attribute("users",
+                        //  Здесь матчер будет работать с сущностями
+                        //  (они сохраняются и достаётся из БД ч/з JPA Hibernate)
+                        //  и мы не можем переопределять equals и hashCode на основе всех полей,
+                        //  т.к. Hibernate будет работать не верно.
+                        //  (У сущностей можно переопределять equals и hashCode только на основе неизменяемых полей)
                         new AssertionMatcher<List<User>>() {
                             @Override
                             public void assertion(List<User> actual) throws AssertionError {
                                 USER_MATCHER.assertMatch(actual, admin, guest, user);
                             }
                         }
+                ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/realEstates"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("realEstates"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/realEstates.jsp"))
+                .andExpect(model().attribute("realEstates",
+                        //  Здесь мы переводим сущности в Dto, а в Dto можно определить equals и hashCode.
+                        getDtos(realEstates)
                 ));
     }
 }
